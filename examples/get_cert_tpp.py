e5c99b0b04033b10374e6c232bd6185940a9a772
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-12-19 18:06:31 +0300

    reformat

--- examples/get_cert_tpp.py
+++ examples/get_cert_tpp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import time
-from vcert import TPPConnection,CertificateRequest
+from vcert import TPPConnection, CertificateRequest
 from pprint import pprint
 from os import environ
 import logging
@@ -15,22 +15,21 @@ ZONE = (environ['TPPZONE'])
 
 
 def main():
-    print("Tring to ping url",URL)
-    conn = TPPConnection(USER,PASSWORD,URL)
+    print("Tring to ping url", URL)
+    conn = TPPConnection(USER, PASSWORD, URL)
     status = conn.ping()
     print("Server online:", status)
     if not status:
         print('Server offline')
         exit(1)
 
-
     request = CertificateRequest(
-                                 common_name=randomword(10)+".venafi.example.com",
-                                 chain_option="first",
-                                 dns_names=["www.client.venafi.example.com", "ww1.client.venafi.example.com"],
-                                 email_addresses="e1@venafi.example.com, e2@venafi.example.com",
-                                 ip_addresses=["127.0.0.1", "192.168.1.1"]
-                                 )
+        common_name=randomword(10) + ".venafi.example.com",
+        chain_option="first",
+        dns_names=["www.client.venafi.example.com", "ww1.client.venafi.example.com"],
+        email_addresses="e1@venafi.example.com, e2@venafi.example.com",
+        ip_addresses=["127.0.0.1", "192.168.1.1"]
+    )
 
     request_id = conn.request_cert(request, ZONE)
     while True:
@@ -44,8 +43,9 @@ def main():
 
 
 def randomword(length):
-   letters = string.ascii_lowercase
-   return ''.join(random.choice(letters) for i in range(length))
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
 
 if __name__ == '__main__':
     main()
