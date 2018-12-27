eeda50e641ca306862c8fe3fbca879aa3d10367a
Aleksandr Rykalin <alexander.rykalin@venafi.com>
2018-12-27 13:45:09 +0300

    fix

--- examples/get_cert.py
+++ examples/get_cert.py
@@ -30,8 +30,8 @@ def main():
     request = CertificateRequest(common_name=randomword(10) + ".venafi.example.com")
     if not isinstance(conn, CloudConnection):
         # cloud connection doesn`t support dns, email and ip in CSR
-        request.dns_names = ["www.client.venafi.example.com", "ww1.client.venafi.example.com"],
-        request.email_addresses = "e1@venafi.example.com, e2@venafi.example.com",
+        request.dns_names = ["www.client.venafi.example.com", "ww1.client.venafi.example.com"]
+        request.email_addresses = "e1@venafi.example.com, e2@venafi.example.com"
         request.ip_addresses = ["127.0.0.1", "192.168.1.1"]
         request.chain_option = "last"
 
